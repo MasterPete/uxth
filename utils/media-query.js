537444ff68d5df1123fc3912dcebd73d5472de7c
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2018-11-02 14:44:47 +0700

    Updates

--- utils/media-query.js
+++
@@ -1,8 +0,0 @@
-const breakpoints = { s: 576, m: 768, l: 992 }
-const mq = Object.entries(breakpoints).reduce(
-  (acc, [key, bp]) => ({ ...acc, [key]: `@media (max-width: ${bp}px)` }),
-  {},
-)
-
-export default mq
-export { breakpoints }
