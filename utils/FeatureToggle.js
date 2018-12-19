cac45b27c8977db65a880fbe08c30cfc010a68b8
ampharino <ampharin@gmail.com>
2018-12-19 20:39:50 +0700

    add flashsale price and text

--- utils/FeatureToggle.js
+++ utils/FeatureToggle.js
@@ -1,5 +1,5 @@
 const isFlashSale = () => {
-    const startDate = new Date('2018-12-19T00:00:00+07:00')
+    const startDate = new Date('2018-12-20T00:00:00+07:00')
     const currentDate = new Date()
     const endDate = new Date('2018-12-21T23:59:59+07:00')
     if (currentDate <= endDate && currentDate >= startDate) {
